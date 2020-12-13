cc77942141bb22d8b063ecf698caa52cd025db0a
developjik <developjik@gmail.com>
2020-12-13 23:05:03 +0900

    3

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {ExamList} = require("./models/ExamList");
 const config = require('./config/key');
 const {auth} = require('./middleware/auth')
 
-// app.enable('trust proxy'); // optional, not needed for secure cookies
+app.set('trust proxy'); // optional, not needed for secure cookies
 // app.use(express.session({
 //     secret : 'somesecret',
 //     key : 'sid',
@@ -52,7 +52,7 @@ app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.  
-  
+
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
